e1cc711040edb1a4d0d94a711102e11683be2dc5
elobaton <138057675+elobaton@users.noreply.github.com>
2023-08-07 21:48:25 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -9,3 +9,20 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let primera = [" super", " mega", " very", "important", "cool"];
+let segunda = ["code", "stack", "tech", "geeks", "duper"];
+let tercera = [".com", ".es", ".us", ".uk", ".com"];
+
+for (let i = 0; i < primera.length; i++) {
+  for (let j = 0; j < segunda.length; j++) {
+    for (let k = 0; k < tercera.length; k++) {
+      let div = document.createElement("div");
+      div.innerText = `
+        ${primera[i]}${segunda[j]}${tercera[k]}
+        `;
+
+      document.body.appendChild(div);
+    }
+  }
+}
